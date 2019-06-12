b31beb5ffd3d3654f8813a83addbf1bb1d3e4d95
Juan Pablo Rojas <juanpablo.rojas@wolox.co>
2019-06-12 13:50:59 -0400

    first attempt

--- spec/wor/soaparty_spec.rb
+++ spec/wor/soaparty_spec.rb
@@ -31,4 +31,39 @@ describe SoapService do
       expect((soap_client.call operation, message).body).to eq(response_body)
     end
   end
+
+  context 'When making a SOAP request with an XML provided' do
+    let(:xml_request) do
+      "<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/'" \
+        "xmlns:tem='http://tempuri.org/'>" \
+          "<soapenv:Header/>" \
+          "<soapenv:Body>" \
+            "<tem:#{operation.capitalize}>" \
+              "<tem:intA>#{message[:intA]}</tem:intA>" \
+              "<tem:intB>#{message[:intB]}</tem:intB>" \
+            "</tem:#{operation.capitalize}>" \
+          "</soapenv:Body>" \
+      "</soapenv:Envelope>"
+    end
+
+    it 'Makes a success request' do
+      operation_result = operation_map.call operation
+      response_body =
+        { "#{operation}_response".to_sym => { "#{operation}_result".to_sym =>
+          operation_result.to_s, :@xmlns => 'http://tempuri.org/' } }
+      byebug
+      # El llamado a call con xml siempre arroja un resultado de 0...
+      expect((soap_client.call operation, xml: xml_request).body).to eq(response_body)
+    end
+  end
+
+  # context 'When making a SOAP request with a local WSDL' do
+  #   it 'Makes a success request' do
+  #   end
+  # end
+
+  # context 'When making a SOAP request with a namespace and endpoint' do
+  #   it 'Makes a success request' do
+  #   end
+  # end
 end
